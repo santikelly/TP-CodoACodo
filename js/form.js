9cb6bd4a316ffe1bdc2e40f7dd211fb5047864af
santikelly <santikelly@gmail.com>
2024-05-22 22:51:06 -0300

    formulario contacto

--- js/form.js
+++ js/form.js
@@ -1,46 +1,146 @@
 let id = (id) => document.getElementById(id);
-
 let classes = (classes) => document.getElementsByClassName(classes);
 
 let username = id("username"),
   email = id("email"),
-  password = id("password"),
+  edad = id("edad"),
+  captcha = id("captcha"),
   form = id("form"),
+  sucursales = id("sucursales")
   
   errorMsg = classes("error"),
   successIcon = classes("success-icon"),
   failureIcon = classes("failure-icon");
 
- 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
+let engine = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
+    // icons
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+    // icons
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
+let validateCaptcha = () => {
+  const correctAnswer = "8";
+  if (captcha.value.trim() !== correctAnswer) {
+    errorMsg[5].innerHTML = "Respuesta de CAPTCHA incorrecta";
+    captcha.style.border = "2px solid red";
+    failureIcon[5].style.opacity = "1";
+    successIcon[5].style.opacity = "0";
+    return false;
+  } else {
+    errorMsg[5].innerHTML = "";
+    captcha.style.border = "2px solid green";
+    failureIcon[5].style.opacity = "0";
+    successIcon[5].style.opacity = "1";
+    return true;
+  }
+};
 
-  let engine = (id, serial, message) => {
+let validateForm = () => {
+  engine(username, 0, "Nombre y apellido no puede estar en blanco");
+  engine(email, 1, "Correo Electrónico no puede estar en blanco");
+  engine(edad, 2, "Edad no puede estar en blanco");
 
-    if (id.value.trim() === "") {
-      errorMsg[serial].innerHTML = message;
-      id.style.border = "2px solid red";
-      console.log('error')
-      // icons
-      failureIcon[serial].style.opacity = "1";
-      successIcon[serial].style.opacity = "0";
-    } 
-    
-    else {
-      errorMsg[serial].innerHTML = "";
-      id.style.border = "2px solid green";
-      console.log('ok')
-      // icons
-      failureIcon[serial].style.opacity = "0";
-      successIcon[serial].style.opacity = "1";
+  validateCheckbox("especie-de-mascota", 3, "Debe seleccionar al menos una especie de mascota");
+  validateRadio("tipo-de-servicio", 4, "Debe seleccionar un tipo de servicio");
+  if (validateCaptcha()) {
+    // Enviar formulario o realizar cualquier otra acción si el CAPTCHA es correcto
+    alert("Formulario enviado correctamente");
+  }
+};
+
+let validateCheckbox = (name, serial, message) => {
+  let checkboxes = document.getElementsByName(name);
+  let checked = false;
+  
+  for (let checkbox of checkboxes) {
+    if (checkbox.checked) {
+      checked = true;
+      break;
     }
   }
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  if (!checked) {
+    errorMsg[serial].innerHTML = message;
+    for (let checkbox of checkboxes) {
+      checkbox.style.outline = "2px solid red";
+    }
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    for (let checkbox of checkboxes) {
+      checkbox.style.outline = "none";
+    }
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
+
+let validateRadio = (name, serial, message) => {
+  let radios = document.getElementsByName(name);
+  let checked = false;
   
-    engine(username, 0, "Username cannot be blank");
-    engine(email, 1, "Email cannot be blank");
-    engine(password, 2, "Password cannot be blank");
-  });
+  for (let radio of radios) {
+    if (radio.checked) {
+      checked = true;
+      break;
+    }
+  }
+
+  if (!checked) {
+    errorMsg[serial].innerHTML = message;
+    for (let radio of radios) {
+      radio.style.outline = "2px solid red";
+    }
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    for (let radio of radios) {
+      radio.style.outline = "none";
+    }
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  engine(username, 0, "Nombre y apellido no puede estar en blanco");
+  engine(email, 1, "Correo electrónico no puede estar en blanco");
+  engine(edad, 2, "Edad no puede estar en blanco");
+  engine(sucursal, 5, "Debe seleccionar una sucursal");
+
+  // Obtener los valores de los checkboxes de especies de mascotas
+  let speciesValues = Array.from(document.querySelectorAll('input[name="especie-de-mascota"]:checked')).map(el => el.value);
+  
+  // Obtener el valor del radio seleccionado de tipo de servicio
+  let serviceType = document.querySelector('input[name="tipo-de-servicio"]:checked');
+
+  // Imprimir los valores en la consola
+  if (speciesValues.length > 0 && serviceType) {
+    console.log("Nombre y apellido:", username.value);
+    console.log("Correo electrónico:", email.value);
+    console.log("Edad:", edad.value);
+    console.log("Especie de mascota:", speciesValues);
+    console.log("Tipo de servicio:", serviceType.value);
+    console.log("Sucursal:", sucursal.value);
+  } else {
+    if (speciesValues.length === 0) {
+      console.log("Especie de mascota no puede estar en blanco");
+    }
+    if (!serviceType) {
+      console.log("Tipo de servicio no puede estar en blanco");
+    }
+  }
+});
